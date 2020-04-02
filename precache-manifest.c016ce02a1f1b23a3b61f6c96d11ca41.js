c07f9fb87a5311e828a3160eecbeab65693e822b
LEMARK-J-MAC <lemark-j-mac@LEMARK-J-MAC.local>
2020-04-01 20:04:31 -0400

    Updates

---
+++ precache-manifest.c016ce02a1f1b23a3b61f6c96d11ca41.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13e6743cc446ea5a28bf743c50b607c7",
+    "url": "/cyborgs-rolodex/index.html"
+  },
+  {
+    "revision": "12781cb9a6dd603ececc",
+    "url": "/cyborgs-rolodex/static/css/main.f56515aa.chunk.css"
+  },
+  {
+    "revision": "a13426a5320ec9232a49",
+    "url": "/cyborgs-rolodex/static/js/2.8e2331d1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cyborgs-rolodex/static/js/2.8e2331d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12781cb9a6dd603ececc",
+    "url": "/cyborgs-rolodex/static/js/main.b2fb49fe.chunk.js"
+  },
+  {
+    "revision": "64103aba4172841fa1cd",
+    "url": "/cyborgs-rolodex/static/js/runtime-main.68f92417.js"
+  }
+]);
